646cc12b7a89bd8f7cf2afa169370a372e2cbedd
ValarieOyieke <99278182+ValarieOyieke@users.noreply.github.com>
2023-02-06 19:29:15 +0300

    update index.js

--- index.js
+++ index.js
@@ -5,25 +5,22 @@ const ratingContainer = document.getElementById('rating-container')
 
 let rating = 0;
 
-document.addEventListener('click', function (event) {
+document.addEventListener('click', (event) => {
   if (event.target.matches('.rate-btn')) {
     const targetBtn = event.target;
     rating = targetBtn.textContent;  
-
-    console.log(targetBtn.textContent);
   }
 });
 
 
-
-submitBtn.addEventListener("click", function() {
-    ratingContainer.innerHTML = `<div class="thankyou-state">
-    <img src="illustration-thank-you.svg"/>
-    <p class="selected">You selected ${rating}
-    out of 5</p>
-    <h2>Thank you!</h2>
-    <p>We appreciate you taking the time to give a rating. If you ever need more support, 
-    don’t hesitate to get in touch!</p>
-</div> `
-console.log("me")
-  });
+submitBtn.addEventListener("click", ()=> {
+    ratingContainer.innerHTML = 
+    `<div class="thankyou-state">
+          <img src="illustration-thank-you.svg"/>
+          <p class="selected">You selected ${rating}
+          out of 5</p>
+          <h2>Thank you!</h2>
+          <p>We appreciate you taking the time to give a rating. If you ever need more support, 
+          don’t hesitate to get in touch!</p>
+    </div> `
+});
