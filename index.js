f2e97a17d48d181e6dcd817267c3fde373d624ca
Valarie Oyieke <oyiekeav@gmail.com>
2023-02-06 18:01:48 +0300

    updates

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+const rateBtn = document.querySelectorAll('.rate-btn')
+const chosenRate = document.getElementById('chosen-rate')
+const submitBtn = document.getElementById('submit-btn')
+const ratingContainer = document.getElementById('rating-container')
